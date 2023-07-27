250895819e77ca5c831c912e33798fd5cc6df779
Leonan Müller <leomullerluiz@gmail.com>
2023-07-27 20:18:02 -0300

    add search

---
+++ api/search.php
@@ -0,0 +1,34 @@
+<?php
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once('../core/initialize.php');
+
+    $product = new Product($db);
+    $product->name = isset($_GET['term']) ? $_GET['term'] : die();
+    $result = $product->search();
+    $num = $result->rowCount();
+
+
+    $product_arr = array();
+    $product_arr['return'] = array();
+    $product_arr['data'] = array();
+
+    if($num > 0){
+        $return_message = array('message' => 'success', 'num_results' => $num); 
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $product_item = array(
+                'id' => $id,
+                'name' => $name
+            );
+            array_push($product_arr['data'], $product_item);
+        }
+    }else{
+        $return_message = array('message' => 'no_result', 'num_results' => 0); 
+        array_push($product_arr['data'], array());  
+    }
+
+    array_push($product_arr['return'], $return_message);
+    print_r(json_encode($product_arr));
--- core/product.php
+++ core/product.php
@@ -20,9 +20,8 @@
         }
 
         public function read_single(){
-            $query = 'SELECT * FROM '.$this->table.' WHERE id = ? LIMIT 1';
+            $query = "SELECT * FROM ".$this->table." WHERE id = ".$this->id." LIMIT 1";
             $stmt = $this->conn->prepare($query);
-            $stmt->bindParam(1, $this->id);
             $stmt->execute();
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
             
@@ -32,6 +31,15 @@
                 return $stmt;
             }
             return false;
+        }
+        
+        public function search(){
+            global $product_table;
+            $query = "SELECT * FROM ".$this->table." WHERE name LIKE '%".$this->name."%'";
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            return $stmt;
+
         }
 
     }//END class
